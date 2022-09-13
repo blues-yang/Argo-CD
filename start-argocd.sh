80826e4a31aa92bf2c7cec481d6f2ca3277ce85f
blues-yang <blyan@microsoft.com>
2022-09-13 11:03:55 +0800

    fix script

--- start-argocd.sh
+++ start-argocd.sh
@@ -14,7 +14,7 @@ echo -e "\e[44m Login myapp(http://localhost:3200/)\e[49m"
 error=`kubectl port-forward -n myapp svc/myapp-service 3200:3200 2>&1`
 echo ❌ $error
 
-while [[ $error == *"lost connection to pod"* ] | [ $error == *"pod not found"* ]]
+while [[ $error == *"lost connection to pod"* ]] || [[ $error == *"pod not found"* ]]
 do
    echo ✅ Reconnect service
    error=`kubectl port-forward -n myapp svc/myapp-service 3200:3200 2>&1`
